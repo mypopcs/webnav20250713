28536c6cc9bccde741217ea9ce70d04202750542
mypopcs <332593916@qq.com>
2025-07-15 01:57:05 +0800

    完成前端后台初始化

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -1,5 +1,18 @@
+// /admin/src/main.ts
 import { createApp } from 'vue'
-import './style.css'
+import { createRouter, createWebHistory } from 'vue-router'
+import ui from '@nuxt/ui/vue-plugin'
 import App from './App.vue'
+import './assets/main.css' // 导入我们即将创建的 CSS 文件
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const router = createRouter({
+  routes: [], // 路由表暂时为空，后续会填充
+  history: createWebHistory()
+})
+
+app.use(router)
+app.use(ui)
+
+app.mount('#app')
--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,7 +1,11 @@
+// /admin/vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ui from '@nuxt/ui/vite' // 导入 NuxtUI Vite 插件
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ui() // 使用插件
+  ]
 })
